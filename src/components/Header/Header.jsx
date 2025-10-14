e0238e3aa093a95dd1615751cb2f600c08703de1
raphaelaluvi <raphaluvi@gmail.com>
2025-10-14 19:44:40 -0300

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,39 +6,39 @@ export default function Header() {
   return (
     <header className={styles.navbar}>
       <div className={styles.logo}>
-        <NavLink to="/">
+        <NavLink to="/" className={styles.logoLink}>
           <img src={logo} alt="CriaKids Logo" className={styles.logoImg} />
         </NavLink>
       </div>
       <nav>
         <ul>
           <li>
-            <NavLink to="/" className={({ isActive }) => isActive ? styles.active : ""}>
+            <NavLink to="/" className={styles.menu}>
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink to="/planos" className={({ isActive }) => isActive ? styles.active : ""}>
+            <NavLink to="/planos" className={styles.menu}>
               Planos
             </NavLink>
           </li>
           <li>
-            <NavLink to="/sobre" className={({ isActive }) => isActive ? styles.active : ""}>
+            <NavLink to="/sobre" className={styles.menu}>
               Sobre Nós
             </NavLink>
           </li>
           <li>
-            <NavLink to="/contato" className={({ isActive }) => isActive ? styles.active : ""}>
+            <NavLink to="/contato" className={styles.menu}>
               Contatos
             </NavLink>
           </li>
           <li>
-            <NavLink to="/login" className={({ isActive }) => isActive ? styles.active : ""}>
+            <NavLink to="/login" className={styles.menu}>
               Login
             </NavLink>
           </li>
           <li>
-            <NavLink to="/cadastro" className={({ isActive }) => isActive ? styles.active : ""}>
+            <NavLink to="/cadastro" className={styles.menu}>
               Cadastro
             </NavLink>
           </li>
